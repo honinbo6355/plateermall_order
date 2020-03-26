e6becb6ab6891c80fe7a3ac5209449a301ad9e43
vastly10 <vastly10@gmail.com>
2020-03-26 16:23:03 +0900

    서비스 모듈 테스트 클래스 작성 후 동작 확인

--- shoppingmall_order/shoppingmall_order_service/src/test/java/com/plateer/OrderServiceTestApplication.java
+++ shoppingmall_order/shoppingmall_order_service/src/test/java/com/plateer/OrderServiceTestApplication.java
@@ -1,4 +1,4 @@
-package com.plateer.service;
+package com.plateer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- shoppingmall_order/shoppingmall_order_service/src/test/java/com/plateer/service/impl/OrderServiceImplTest.java
+++ shoppingmall_order/shoppingmall_order_service/src/test/java/com/plateer/service/impl/OrderServiceImplTest.java
@@ -1,9 +1,11 @@
 package com.plateer.service.impl;
 
-import com.plateer.service.OrderServiceTestApplication;
+import com.plateer.OrderServiceTestApplication;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -11,9 +13,16 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest(classes = OrderServiceTestApplication.class)
 public class OrderServiceImplTest {
 
+    @Autowired
+    private OrderServiceImpl orderService;
+
+    @Before
+    public void before(){
+
+    }
+
     @Test
     public void test(){
-
-        Assert.assertTrue(true);
+        Assert.assertEquals(4 , orderService.findAllOrderFromUserId("testid").size());
     }
 }
